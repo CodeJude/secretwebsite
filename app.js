461e6cf3625d1689b010c94818f9defc0fd3bbbf
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2022-11-01 15:49:34 +0400

    Hashing The Passwords

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 const PORT =process.env.PORT || 4000;
 
 
 const app = express();
+
+console.log(md5("123456"));
 
 
 app.set('view engine', 'ejs');
@@ -21,7 +23,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET_KEY, encryptedFields: ['password'] });
 
 
 const User = new mongoose.model("User", userSchema)
@@ -40,7 +41,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -54,7 +55,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err) {
@@ -62,12 +63,8 @@ app.post("/login", function(req, res){
         } else {
             if (foundUser) {
                 if (foundUser.password === password) {
-                    res.render("secrets")
-                } else {
-                    res.send("password does not match")
-                }
-            } else {
-                res.send("Login your details")
+                    res.render("secrets");
+            } 
             }
         }
     });
